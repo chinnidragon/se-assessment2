49e6d79554f60201d1b30f829accba3f7208910f
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2026-06-27 10:48:26 +0000

    touching up styling in index page

--- setupFolder.sh
+++ setupFolder.sh
@@ -1,4 +1,4 @@
-#Adapted from the tutorial
+#Adapted to my project from the tutorial
 
 # Check if exactly two parameters were provided
 if [ $# -ne 2 ]; then
@@ -26,13 +26,30 @@ mkdir -p "$DEST/templates"
 touch "$DEST/app.py"
 
 # Copy files from the source folder to the destination/static folder
+
+#All the PWA files
 cp "$SOURCE/style.css" "$DEST/static/"
 cp "$SOURCE/script.js" "$DEST/static/"
 cp "$SOURCE/bigIcon.png" "$DEST/static/"
 cp "$SOURCE/favicon.ico" "$DEST/static/"
 cp "$SOURCE/manifest.json" "$DEST/static/"
 cp "$SOURCE/service-worker.js" "$DEST/static/"
+
+#All the images
+
+#All the templates
+cp "$SOURCE/charsheets.html" "$DEST/templates/"
+cp "$SOURCE/create_event.html" "$DEST/templates/"
+cp "$SOURCE/create_notices.html" "$DEST/templates/"
+cp "$SOURCE/dice.html" "$DEST/templates/"
+cp "$SOURCE/edit_profile.html" "$DEST/templates/"
 cp "$SOURCE/index.html" "$DEST/templates/"
-cp "$SOURCE/page1.html" "$DEST/templates/"
+cp "$SOURCE/login.html" "$DEST/templates/"
+cp "$SOURCE/notes.html" "$DEST/templates/"
+cp "$SOURCE/notices.html" "$DEST/templates/"
+cp "$SOURCE/profile.html" "$DEST/templates/"
+cp "$SOURCE/signup.html" "$DEST/templates/"
+cp "$SOURCE/timetable.html" "$DEST/templates/"
+cp "$SOURCE/view_chars.html" "$DEST/templates/"
 
 echo "Files have been successfully copied from '$SOURCE' to '$DEST'."
